484e5950e831a47ba7448c505e0090120d414490
unknown <jsaicharanreddy0605@gmail.com>
2025-04-09 21:16:54 +0530

    added output as comments

--- assignment_day3.py
+++ assignment_day3.py
@@ -1,12 +1,10 @@
 lst = [1,2,3, [1,2,3,[3,4,[4,3]],2]]
 lst1 = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
 
-def flatten(lst):
-    flatten_list=[]
-    i=0
+def flatten(lst,flatten_list=[]):
     for i in lst:
         if type(i) is list:
-            flatten_list+= flatten(i)#recursion
+            flatten(i,flatten_list)
         else:
             flatten_list.append(i)
     return flatten_list
@@ -16,7 +14,7 @@ def convert(lst):
     out=[]
     for i in lst:
         if type(i) is list:
-            out.append(convert(i))#recursion
+            out.append(convert(i))
         else:
             out.append([int(i) for i in list(i) if i.isdigit()])# list comprenhesion to append iff it is digit
     return out
@@ -26,4 +24,9 @@ print(flatten(lst))
 
 
 print(convert(lst1))
+
+
+#output
+#[1, 2, 3, 1, 2, 3, 3, 4, 4, 3, 2]
+#[[[[0, 1, 2], [3, 4, 5]], [[5, 6, 7], [9, 4, 2]]]]
     
