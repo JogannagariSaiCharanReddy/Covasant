07a2cb68300c3d84a94c94aacc9a2168bc83c074
unknown <jsaicharanreddy0605@gmail.com>
2025-04-10 23:46:03 +0530

    added day4 quesion 6

--- day4/day4_question6.py
+++ day4/day4_question6.py
@@ -1,32 +1,43 @@
 import glob
 import os
-import datetime
+from datetime import datetime,date
+import time
 
 class File():
     def __init__(self,path):
         self.path =path
+        
+    def get_files(self,path,files_dic={}):
+        files = glob.glob(os.path.join(path, "*"))
+        for file in files:
+            if os.path.isfile(file):
+                files_dic[file] = os.path.getsize(file)
+            elif os.path.isdir(file):
+                self.get_files(file, files_dic)
+        return files_dic
        
     def getMaxSizeFile(self,n=1):
-        def get_files(path, files_dic={}):
-            files = glob.glob(os.path.join(path, "*"))
-            for file in files:
-                if os.path.isfile(file):
-                    files_dic[file] = os.path.getsize(file)
-                elif os.path.isdir(file):
-                    get_files(file, files_dic)
-            return files_dic
-        files=get_files(self.path)
-       
-        print(sorted(files,key=lambda file:files[file])[-n:])
+        files=self.get_files(self.path)
+        return sorted(files,key=lambda file:files[file])[-n:]
         
-    #def getLatestFiles(self,date):
-    #    print(date)
+    def getLatestFiles(self,cdate):
+        files=self.get_files(self.path)
+        d = datetime.combine(cdate,datetime.min.time())
+        seconds = int(d.timestamp())
+        latest_files={file:os.path.getctime(file)
+                        for file in files
+                                 if os.path.getctime(file)>seconds}
+        return list(latest_files.keys())
+        
+            
+        
+        
 
         
         
         
 
 fs = File(r"C:\Users\Sai Charan\Desktop\Python\assignments")
-fs.getMaxSizeFile(2) # gives two max file names 
-#fs.getLatestFiles(datetime.date(2018,2,1))
+print(fs.getMaxSizeFile(2)) # gives two max file names 
+print(fs.getLatestFiles(date(2025,4,9)))
 #Returns list of files after 1st Feb 2018 
