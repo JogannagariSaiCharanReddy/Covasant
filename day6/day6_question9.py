d85144269f057ce418dedac114b17975f186a383
unknown <jsaicharanreddy0605@gmail.com>
2025-04-12 21:49:29 +0530

    added day 6th assignment

---
+++ day6/day6_question9.py
@@ -0,0 +1,34 @@
+import asyncio 
+import time, sys
+import aiohttp
+import requests
+from bs4 import BeautifulSoup
+
+
+async def download(url):
+    async with aiohttp.ClientSession() as sess:
+        async with sess.get(url) as resp:
+            return await resp.text()
+            
+
+async def get_urls(url):
+    print(url)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup.find_all("a",href=True)
+    
+async def download_all_urls(url):
+    urls=[i.get("href") for i in await get_urls(url) if "http" in i.get("href") ]
+    tasks= [asyncio.create_task(download(url)) for url in urls]
+    return [await task for task in tasks]
+    
+    
+    
+    
+if __name__=='__main__':
+    url="http://notepadfromdas.pythonanywhere.com/pad/share"
+    st=time.time()
+    res = asyncio.run(download_all_urls(url)) 
+    print("Time taken :",time.time()-st,"sec")
+    print(len(''.join(res)))
+    
